cce4f839004694cef018f11b39d61eb0b6b8c395
MaximeLach <maxime.lach@gmail.com>
2016-10-13 11:01:10 +0200

    ModuleJavScript v2

---
+++ public_html/JS/evenements.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function cliqueBouton(){
+//    var divTitre = document.getElementById("titre");
+//    divTitre.innerHTML = "Yo man!";
+    
+//    var liste = document.getElementsByClassName("menu");
+//    for(i=0; i<liste.length;i++){
+//        var tag = liste[i];
+//        tag.innerHTML = "Coucou";
+//    }
+
+
+}
+
+function cliqueBouton2(){
+    var input = document.getElementById("texte");
+    var ligneTitre = document.getElementById("titre");
+    var bouton = document.getElementById("bouton1");
+  
+    ligneTitre.innerHTML = input.value.toUpperCase();
+
+}
